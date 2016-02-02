160ea12d728e2debdb3c634b048c56f832dcf3d3
Fernando Cejas <fcejas@gmail.com>
2016-02-02 18:11:03 +0100

    Add See annotation

---
+++ src/main/java/com/fernandocejas/arrow/annotations/See.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2016 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fernandocejas.arrow.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Denotes that the class, method or field is linked to a link.
+ * A ref value should be added in order to specify the link url.
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface See {
+  String ref();
+}
